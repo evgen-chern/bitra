6272d2177c56548539bd396c6fe75292171c4e21
evgen <chernous.eu@gmail.com>
2025-03-26 00:53:53 +0300

    Files add

---
+++ local/php_interface/include/site_closed.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <div style="margin: 50px auto">
+        <p style="text-align:center; font-size: 24px; font-weight: bold; color: #1c72aa;">Сайт находится на стадии разработки.</p>
+    </div>
+</body>
+</html>
+
---
+++ local/php_interface/this_site_support.php
@@ -0,0 +1,3 @@
+<div style="display: inline-flex; flex-direction: row; align-items: center">
+    <a class="a" href="https://t.me/evgen_chern">Разработка сайта - Evgen</a>
+</div>
